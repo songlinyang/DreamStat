4d01b9a894e5512ad7d8f922ebfbc71553d2bc30
mac <yang_song_lin@163.com>
2025-09-03 10:35:01 +0800

    task3 hook作业2

--- GoTask3/Blog/blog.go
+++ GoTask3/Blog/blog.go
@@ -9,7 +9,6 @@ import (
 
 type User struct {
 	gorm.Model
-	ID         uint
 	UserName   string
 	Password   string
 	Email      string
@@ -18,7 +17,6 @@ type User struct {
 }
 type Post struct {
 	gorm.Model
-	ID            uint
 	Title         string
 	Body          string
 	UserID        uint
@@ -27,7 +25,6 @@ type Post struct {
 }
 type Comments struct {
 	gorm.Model
-	ID             uint
 	Content        string
 	PostID         uint
 	GoodStateCount uint `gorm:"default:0"`
@@ -115,14 +112,18 @@ func main() {
 
 	//题目3：钩子函数
 	//3.1为 Post 模型添加一个钩子函数，在文章创建时自动更新用户的文章数量统计字段。
-	if err := db.Select("user_id", "title", "body").Create(&Post{UserID: 1, Title: "你好", Body: "你好"}).Error; err != nil {
-		panic(err)
-	}
+	//if err := db.Select("user_id", "title", "body").Create(&Post{UserID: 1, Title: "你好", Body: "你好"}).Error; err != nil {
+	//	panic(err)
+	//}
 	//3.2为 Comment 模型添加一个钩子函数，在评论删除时检查文章的评论数量，如果评论数量为 0，则更新文章的评论状态为 "无评论"
+	var comment []Comments
+	db.Where("post_id", 2).Find(&comment)
+	db.Debug().Where("post_id", 2).Delete(&comment) //触发delete钩子函数，不使用事务直接删除
 }
 
 // 创建钩子
 // 开始事务
+
 func (p *Post) BeforeSave(tx *gorm.DB) error {
 	fmt.Println("BeforeSave")
 	//if u.Name != "Jinzhu" {
@@ -132,6 +133,7 @@ func (p *Post) BeforeSave(tx *gorm.DB) error {
 }
 
 // BeforeSave, BeforeCreate, AfterSave, AfterCreate
+
 func (p *Post) BeforeCreate(tx *gorm.DB) (err error) {
 	fmt.Println("BeforeCreate")
 	return nil
@@ -145,31 +147,54 @@ func (p *Post) AfterCreate(tx *gorm.DB) (err error) {
 	return nil
 }
 
-// 关联前的 save
-// 插入记录至 db
-// 关联后的 save
-type PostsCountResult struct {
-	PostsCount int `gorm:"column:posts_count"`
+//关联前的 save
+//插入记录至 db
+//关联后的 save
+//
+//type PostsCountResult struct {
+//	PostsCount int `gorm:"column:posts_count"`
+//}
+//
+//func (p *Post) AfterSave(tx *gorm.DB) (err error) {
+//	fmt.Println("AfterSave")
+//	fmt.Println("BeforeCreate")
+//	//先查出有多少次
+//	var user User
+//	var postsCountResult PostsCountResult
+//	if err := tx.Select("posts_count").Where("id=?", p.UserID).First(&user).Scan(&postsCountResult).Error; err != nil {
+//		panic(err)
+//	}
+//	fmt.Println("当前用户文章数：", postsCountResult.PostsCount)
+//	postsCountResult.PostsCount = postsCountResult.PostsCount + 1
+//	if err := tx.Debug().Model(&user).Where("id=?", p.UserID).Update("posts_count", postsCountResult.PostsCount).Error; err != nil {
+//		panic(err)
+//	}
+//	fmt.Println("user.PostsCount累计：", postsCountResult.PostsCount)
+//	fmt.Println("hhhh=>", user)
+//	//if u.Name != "Jinzhu" {
+//	//	return errors.New("invalid role")
+//	//}
+//	return err
+//}
+
+type CommentCountReuslt struct {
+	CommentCount int `gorm:"column:comment_count"`
 }
 
-func (p *Post) AfterSave(tx *gorm.DB) (err error) {
-	fmt.Println("AfterSave")
-	fmt.Println("BeforeCreate")
-	//先查出有多少次
-	var user User
-	var postsCountResult PostsCountResult
-	if err := tx.Select("posts_count").Where("id=?", p.UserID).First(&user).Scan(&postsCountResult).Error; err != nil {
-		panic(err)
+func (c *Comments) BeforeDelete(tx *gorm.DB) (err error) {
+	return nil
+
+}
+
+func (c *Comments) AfterDelete(tx *gorm.DB) (err error) {
+	// 如果 c.PostID 为空，则在 DB 中读取完整记录（依赖传入的主键 ID）
+	// 查询文章当前评论数量并按需更新（注意列名/字段应一致）
+	var commentCount int64
+	tx.Debug().Model(&Comments{}).Where("post_id = ?", c.PostID).Count(&commentCount)
+	if commentCount == 0 {
+		tx.Debug().Model(&Post{}).Where("id = ?", c.PostID).Update("comments_status", "无评论")
+	} else {
+		fmt.Println("仍存在评论，不进行设置！")
 	}
-	fmt.Println("当前用户文章数：", postsCountResult.PostsCount)
-	postsCountResult.PostsCount = postsCountResult.PostsCount + 1
-	if err := tx.Debug().Model(&user).Where("id=?", p.UserID).Update("posts_count", postsCountResult.PostsCount).Error; err != nil {
-		panic(err)
-	}
-	fmt.Println("user.PostsCount累计：", postsCountResult.PostsCount)
-	fmt.Println("hhhh=>", user)
-	//if u.Name != "Jinzhu" {
-	//	return errors.New("invalid role")
-	//}
-	return err
+	return nil
 }
